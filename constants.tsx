3696f31abeeee595337ca6ab28f5f2a5dbd070bf
Vivekipm2802 <ipmcareeronline@gmail.com>
2026-04-01 20:19:40 +0530

    Create constants.tsx

---
+++ constants.tsx
@@ -0,0 +1,10 @@
+export const COLORS = {
+  bg: '#020617',
+  card: '#081225',
+  neonCyan: '#00F0FF',
+  turfGreen: '#39FF14',
+  gold: '#FFD000',
+  amber: '#FF8C00',
+  accentRed: '#FF004D',
+  subtle: '#ffffff50',
+};
